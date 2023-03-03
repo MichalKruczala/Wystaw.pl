1d2c8cdeca3d2c2689ea8638190716c20f777528
Michał <kruczalamichal@gmail.com>
2023-03-03 21:39:31 +0100

    da się uploadować fotę,zobaczyć ją i przesłać furmularzem :)

--- src/main/java/pl/it/portfolio/controllers/TryController.java
+++ src/main/java/pl/it/portfolio/controllers/TryController.java
@@ -1,31 +1,33 @@
 package pl.it.portfolio.controllers;
-
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.Part;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
 @Controller
 public class TryController {
-    @RequestMapping(path = "/try", method = RequestMethod.GET)
-    public String sproboj() {
-        return "proba2";
+    @RequestMapping(path = "/tryh", method = RequestMethod.GET)
+    public String sprobojg() {
+        return "proba2x";
+    }@RequestMapping(path = "/tryn", method = RequestMethod.GET)
+    public String sprobojh() {
+        return "probax";
     }
 
+    @RequestMapping(path = "/try", method = RequestMethod.GET)
+    public String sproboj() {
+        return "pomiedzy";
+    }
 
     @RequestMapping(path = "/try", method = RequestMethod.POST)
     public String sproboj2(HttpServletRequest request) {
-        System.out.println(request.getHeaderNames());
-        System.out.println("przyszło");
+       // System.out.println(request.getHeaderNames());
+       // System.out.println("przyszło");
         try {
             Part file = request.getPart("image");
             String imageFileName = file.getSubmittedFileName();
